54a476b99add6ec4fd6583640ea9982cbe53a8d9
chetra-seng <sengchetra.love@icloud.com>
2024-02-18 14:31:02 +0700

    Added not found page

---
+++ apps/client/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="h-[calc(100vh-15rem)] flex flex-col justify-center items-center">
+      <h1 className="!leading-[1.5] text-2xl sm:text-4xl font-bold">404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
+    <footer className="mb-10 px-4 text-center text-gray-500 dark:text-white/80">
       <small className="mb-2 text-xs block">
         &copy; 2024 <Link href={"/"}>Chetra Seng</Link>. All rights reserved.
       </small>
