047a0d6710047ec935806b8d28faf0967b79eb6c
chetra-seng <sengchetra.love@icloud.com>
2024-02-20 01:15:12 +0700

    Added backoffice theme

--- apps/server/sanity.config.tsx
+++ apps/server/sanity.config.tsx
@@ -2,13 +2,22 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+const {theme} = (await import(
+  // @ts-expect-error -- TODO setup themer.d.ts to get correct typings
+  'https://themer.sanity.build/api/hues'
+)) as {theme: import('sanity').StudioTheme}
+
+const Logo = () => {
+  return <img src={'/static/logo.png'} alt="brand logo" style={{height: '100%', width: '100%'}} />
+}
 
 export default defineConfig({
   name: 'default',
   title: 'personal-portfolio',
-
+  icon: Logo,
   projectId: 'j493y206',
   dataset: 'production',
+  theme,
 
   plugins: [structureTool(), visionTool()],
 
