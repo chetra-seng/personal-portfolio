1b5921d9cd5d7e4f13e5c9176ae9b3e0940a1380
chetra-seng <sengchetra.love@icloud.com>
2024-02-19 18:19:16 +0700

    Updated read me

--- apps/server/schemas/skill.ts
+++ apps/server/schemas/skill.ts
@@ -1,30 +1,30 @@
-import { defineType } from "sanity";
+import {defineType} from 'sanity'
 
 export default defineType({
-  name: "skill",
-  title: "Skill",
-  type: "document",
+  name: 'skill',
+  title: 'Skill',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Name",
-      description: "The name of the skill",
-      type: "string"
+      name: 'name',
+      title: 'Name',
+      description: 'The name of the skill',
+      type: 'string',
     },
     {
-      name: "level",
-      title: "Level",
-      description: "The level of the skill in percentage (0-100)",
-      type: "number"
+      name: 'level',
+      title: 'Level',
+      description: 'The level of the skill in percentage (0-100)',
+      type: 'number',
     },
     {
-      name: "icon",
-      title: "Icon",
-      description: "The icon of the skill",
-      type: "image",
+      name: 'icon',
+      title: 'Icon',
+      description: 'The icon of the skill',
+      type: 'image',
       options: {
-        hotspot: true
-      }
-    }
-  ]
+        hotspot: true,
+      },
+    },
+  ],
 })
