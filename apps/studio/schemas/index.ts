64e97aea6ac90997e5b07631dcd6d86a651c5e13
chetra-seng <sengchetra.love@icloud.com>
2024-03-10 06:03:23 +0700

    Split sanity schema into package

--- apps/studio/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import bioinfo from './bioInfo'
-import contact from './contact'
-import navitem from './navitem'
-import project from './project'
-import skill from './skill'
-import social from './social'
-import experience from './experience'
-
-export const schemaTypes = [bioinfo, experience, contact, navitem, project, social, skill]
