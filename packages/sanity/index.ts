8b7797b442f82ec927956fa4de87715b54e05cd8
chetra-seng <sengchetra.love@icloud.com>
2024-03-13 23:33:55 +0700

    Added seo schema

--- packages/sanity/index.ts
+++ packages/sanity/index.ts
@@ -3,10 +3,11 @@ import contact from "./schemas/contact";
 import experience from "./schemas/experience";
 import navitem from "./schemas/navitem";
 import project from "./schemas/project";
+import seo from "./schemas/seo";
 import social from "./schemas/social";
 import skill from "./schemas/skill";
 
-export const schemaTypes = [bioInfo, contact, experience, navitem, project, social, skill];
+export const schemaTypes = [bioInfo, contact, experience, navitem, project, seo, social, skill];
 
 export * as bioinfo from "./schemas/bioInfo";
 export * as contact from "./schemas/contact";
@@ -15,6 +16,7 @@ export * as navitem from "./schemas/navitem";
 export * as project from "./schemas/project";
 export * as social from "./schemas/social";
 export * as skill from "./schemas/skill";
+export * as seo from "./schemas/seo";
 
 export * from "sanity";
 export * as structure from "sanity/structure";
