d562aca67053570efbc0660d3b7bdfd301d2af5c
chetra-seng <sengchetra.love@icloud.com>
2024-03-15 00:09:42 +0700

    Updated sanity cli config

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_DATASET,
   },
 })
