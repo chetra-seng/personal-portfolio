aebed4963c8372b8ec4a6ab267363cebf2b4b9e6
Chetra Seng <dev@chetraseng.com>
2025-05-22 00:40:09 +0700

    Config and UI updates
    - Revert back to original skill design with some improvement
    - Updated next js config

--- apps/web/src/components/SkillSection.tsx
+++ apps/web/src/components/SkillSection.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { HoverCardArrow } from "@radix-ui/react-hover-card";
-import { IconCloud } from "./magicui/icon-cloud";
+import Image from "next/image";
 
 type Props = {
   skills: Skill[];
@@ -41,31 +41,34 @@ const SkillSection: React.FC<Props> = ({ skills }) => {
         viewport={{ once: true }}
         custom={index}
       >
-        <HoverCard>
-          <HoverCardTrigger asChild>
-            <button className="cursor-pointer rounded-xl border border-black/[0.1] bg-white px-5 py-3 dark:bg-white/10 dark:text-white/80">
+        <div className="group flex items-center rounded-xl border border-black/[0.1] bg-white px-5 py-3 dark:bg-white/10 dark:text-white/80">
+          <Image
+            className="mr-2 flex-shrink-0"
+            width={20}
+            height={20}
+            alt={skill.name}
+            src={`https://cdn.simpleicons.org/${skill.slug}`}
+            unoptimized
+          />
+
+          <div className="relative group w-full min-w-[3rem] overflow-hidden">
+            {/* Skill Name */}
+            <span className="block transition-all duration-500 ease-in-out transform group-hover:translate-y-[-100%] group-hover:opacity-0">
               {skill.name}
-            </button>
-          </HoverCardTrigger>
-          <HoverCardContent className="w-64">
-            <HoverCardArrow asChild>
-              <div className="border-x-[6px] border-t-8 border-solid border-x-transparent border-t-gray-400 dark:border-t-gray-500" />
-            </HoverCardArrow>
-            <div className="flex justify-center space-x-4 text-left">
-              <Avatar className="bg-white dark:bg-white/10">
-                <AvatarImage src={`${skill.skillLogo}?h=100`} />
-                <AvatarFallback>CS</AvatarFallback>
-              </Avatar>
-              <div className="space-y-1">
-                <h4 className="text-sm font-semibold">{skill.name}</h4>
-                <p className="text-sm">
-                  Skill proficiency:{" "}
-                  <span className="font-semibold">{skill.level}%</span>
-                </p>
+            </span>
+
+            {/* Progress Bar */}
+            <div className="absolute left-0 top-0 w-full h-full flex items-center gap-2 opacity-0 transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
+              <div className="relative w-40 h-1.5 bg-gray-200 dark:bg-white/20 rounded-full overflow-hidden">
+                <div
+                  className="h-full rounded-full transition-all duration-700 ease-in-out bg-gradient-to-r from-purple-500 via-blue-500 to-cyan-400"
+                  style={{ width: `${skill.level}%` }}
+                />
               </div>
+              <span className="text-sm">{skill.level}%</span>
             </div>
-          </HoverCardContent>
-        </HoverCard>
+          </div>
+        </div>
       </motion.li>
     ));
   }, [skills, skillAnimation]);
@@ -80,7 +83,9 @@ const SkillSection: React.FC<Props> = ({ skills }) => {
       className="mb-28 max-w-212 scroll-mt-28 text-center sm:mb-40"
     >
       <h2 className="section-header text-center">Skills</h2>
-      <IconCloud images={skills.map(skill => `https://cdn.simpleicons.org/${skill.slug}`)} />
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillList}
+      </ul>
     </section>
   );
 };
